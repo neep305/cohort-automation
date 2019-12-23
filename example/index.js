f9f703a20c0f2c3be88d3e730515c4e9693841a1
neep305 <neep305@gmail.com>
2019-12-24 01:13:16 +0900

    add csv-parse

--- example/index.js
+++ example/index.js
@@ -1,28 +1,50 @@
-var request = require('request');
-var fs = require('fs');
+const request = require('request');
+const fs = require('fs');
+const parse = require('csv-parse');
 
-  request(
+const ws = fs.createWriteStream('cohort-test.csv');
+const parser = parse({delimiter: ','}, (err, records, info) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(records);
+    }
+
+    ws.close();
+});
+
+ws.on('finish', () => {
+    console.log('ws finish: ', ws.path);
+    fs.createReadStream(ws.path).pipe(parser);
+});
+
+ws.on('error', (err) => {
+    console.log(err);
+});
+
+request(
     { method: 'GET'
     , uri: 'https://amplitude.com/api/5/cohorts/request/VQipfz/file'
     , auth: {user:'56d54c6d801092b4e8718b40c87a5441', pass:'0ebde0387b79b1822b674d2667525ce2'}
     }
   , function (error, response, body) {
-      // body is the decompressed response body
-      console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
-      console.log('the decoded data is: ' + body)
-
-      
-        response.pipe(fs.createWriteStream('test.csv'));
+        // body is the decompressed response body
+        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
+        // console.log(response);
+        console.log('the decoded data is: ' + body)
+        
     }
-  )
-  .on('data', function(data) {
+)
+.on('data', function(data) {
     // decompressed data as it is received
-    console.log('decoded chunk: ' + data)
-  })
-  .on('response', function(response) {
+    console.log('2. decoded chunk: ' + data)
+})
+.on('response', function(response) {
+    // const ws = fs.createWriteStream('cohort.csv');
+    response.pipe(ws);
     // unmodified http.IncomingMessage object
     response.on('data', function(data) {
-      // compressed data as it is received
-      console.log('received ' + data.length + ' bytes of compressed data')
+        // compressed data as it is received
+        console.log('1. received ' + data.length + ' bytes of compressed data')
     })
-  })
+})
